3184db22fd614482f780054e6a7f898de812db99
Spank <spank000@gmail.com>
2018-01-28 17:34:02 -0600

    Inicio del Proyecto

---
+++ AppNinja_Grocery_Restful/AppNinja_Grocery_Restful/Controllers/UsuariosController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AppNinja_Grocery_Restful.Controllers
+{
+    public class Person
+
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Twitter { get; set; }
+
+    }
+    public class UsuariosController : ApiController
+    {
+        // GET api/Usuarios
+        public List<Person> GetPerson()
+        {
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person { Name = "Julio", LastName = "Avellaneda", Twitter = "@julitogtu" });
+            persons.Add(new Person { Name = "Juan", LastName = "Ruiz", Twitter = "@juankruiz" });
+            persons.Add(new Person { Name = "Roberto", LastName = "Alvarado", Twitter = "@ralvaradot" });
+            persons.Add(new Person { Name = "Nicolas", LastName = "Herrera", Twitter = "@nicolocodev" });
+            persons.Add(new Person { Name = "Jorge", LastName = "Ramirez", Twitter = "@jramirezdev" });
+            return persons;
+        }
+    }
+}
---
+++ AppNinja_Grocery_Restful/AppNinja_Grocery_Restful/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppNinja_Grocery_Restful.WebApiApplication" Language="C#" %>
